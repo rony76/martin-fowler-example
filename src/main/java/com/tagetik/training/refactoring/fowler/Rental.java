39813f78da7efe1203c05b47b094eafac6e705a1
Rony Cesana <rony.cesana@gmail.com>
2016-03-08 18:26:49 +0100

    Extract Method, Move Method: getFrequentRenterPoints

--- src/main/java/com/tagetik/training/refactoring/fowler/Rental.java
+++ src/main/java/com/tagetik/training/refactoring/fowler/Rental.java
@@ -37,4 +37,13 @@ public class Rental {
 
         return 0;
     }
+
+    public int getFrequentRenterPoints() {
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+
+    }
 }
