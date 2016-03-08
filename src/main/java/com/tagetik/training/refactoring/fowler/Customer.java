aa6c131064fd332ccf3912c9ed2ef428ad918f2d
Rony Cesana <rony.cesana@gmail.com>
2016-03-08 18:02:13 +0100

    Move method: Rental.getCharge()

--- src/main/java/com/tagetik/training/refactoring/fowler/Customer.java
+++ src/main/java/com/tagetik/training/refactoring/fowler/Customer.java
@@ -25,7 +25,7 @@ public class Customer {
         String result = "Rental Record for " + getName() + "\n";
 
         for (Rental rental : rentals) {
-            double thisAmount = amountFor(rental);
+            double thisAmount = rental.getCharge();
 
             // add frequent renter points
             frequentRenterPoints++;
@@ -47,28 +47,4 @@ public class Customer {
         return result;
     }
 
-    private double amountFor(Rental rental) {
-        double result = 0;
-
-        // determine amounts for each line
-        switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (rental.getDaysRented() > 2) {
-                    result += (rental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-
-            case Movie.NEW_RELEASE:
-                result += rental.getDaysRented() * 3;
-                break;
-
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (rental.getDaysRented() > 3) {
-                    result += (rental.getDaysRented() - 3) * 1.5;
-                }
-        }
-        return result;
-    }
 }
--- src/main/java/com/tagetik/training/refactoring/fowler/Rental.java
+++ src/main/java/com/tagetik/training/refactoring/fowler/Rental.java
@@ -16,4 +16,25 @@ public class Rental {
     public int getDaysRented() {
         return daysRented;
     }
+
+    public double getCharge() {
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                if (getDaysRented() > 2) {
+                    return 2.0 + (getDaysRented() - 2) * 1.5;
+                }
+                return 2.0;
+
+            case Movie.NEW_RELEASE:
+                return getDaysRented() * 3.0;
+
+            case Movie.CHILDRENS:
+                if (getDaysRented() > 3) {
+                    return 1.5 + (getDaysRented() - 3) * 1.5;
+                }
+                return 1.5;
+        }
+
+        return 0;
+    }
 }
