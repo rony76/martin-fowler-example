0ce84f808b1583772e4c3a90802868a3cdf68385
Rony Cesana <rony.cesana@gmail.com>
2016-03-08 10:02:12 +0100

    Import iniziale

---
+++ src/main/java/com/tagetik/training/refactoring/fowler/Customer.java
@@ -0,0 +1,69 @@
+package com.tagetik.training.refactoring.fowler;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Customer {
+    private String name;
+    private List<Rental> rentals = new LinkedList<Rental>();
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public void addRental(Rental rental) {
+        rentals.add(rental);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        String result = "Rental Record for " + getName() + "\n";
+
+        for (Rental rental : rentals) {
+            double thisAmount = 0;
+
+            // determine amounts for each line
+            switch (rental.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (rental.getDaysRented() > 2) {
+                        thisAmount += (rental.getDaysRented() - 2) * 1.5;
+                    }
+                    break;
+
+                case Movie.NEW_RELEASE:
+                    thisAmount += rental.getDaysRented() * 3;
+                    break;
+
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (rental.getDaysRented() > 3) {
+                        thisAmount += (rental.getDaysRented() - 3) * 1.5;
+                    }
+            }
+
+            // add frequent renter points
+            frequentRenterPoints++;
+
+            // add bonus for a two day new release rental
+            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1) {
+                frequentRenterPoints++;
+            }
+
+            // show figures for this rental
+            result += "\t" + rental.getDaysRented() + "\t" + thisAmount + "\n";
+            totalAmount += thisAmount;
+        }
+
+        // add footer lines
+        result += "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + frequentRenterPoints + " frequent renter points";
+
+        return result;
+    }
+}
