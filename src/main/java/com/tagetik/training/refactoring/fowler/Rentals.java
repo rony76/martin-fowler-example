410dd13bc8c0634eb3f7c52ebe21bebc04ec8dcf
Rony Cesana <rony.cesana@gmail.com>
2016-04-07 23:03:17 +0200

    Replace Inheritance with Delegation: after

--- src/main/java/com/tagetik/training/refactoring/fowler/Rentals.java
+++ src/main/java/com/tagetik/training/refactoring/fowler/Rentals.java
@@ -1,6 +1,13 @@
 package com.tagetik.training.refactoring.fowler;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
-public class Rentals extends LinkedList<Rental> {
+public class Rentals implements Iterable<Rental> {
+    private LinkedList<Rental> list;
+
+    @Override
+    public Iterator<Rental> iterator() {
+        return list.iterator();
+    }
 }
